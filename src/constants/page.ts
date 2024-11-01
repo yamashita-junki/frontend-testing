5a77e26878302de328f879a5bdc39dc2bde0f78b
j-yamashita <j-yamashita@pro-vision.jp>
2024-11-01 15:27:06 +0900

    Revert "failure commit"
    This reverts commit 9a0bbf67db9ea9c4d7244f46192a0d811b4d9e7f.

--- src/constants/page.ts
+++ src/constants/page.ts
@@ -1,6 +1,6 @@
 const TITLE = 'JSONPlaceholder Users';
 const LOADING_TEXT = 'Loading users...';
-const NO_USERS_TEXT = 'noooooo';
+const NO_USERS_TEXT = 'No users found matching your search.';
 const BACK_TEXT = 'BACK';
 
 export default {
